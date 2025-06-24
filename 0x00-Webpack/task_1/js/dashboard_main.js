34fa3b210b23d59399260e997d7017f5d40274cc
atck3r <androidmini92@gmail.com>
2025-06-24 19:59:13 +0300

    Add Task 1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+var $ = require( "jquery" );
+var _ = require('lodash');
+
+window.jQuery = $;
+window.$ = $;
+var count = 0;
+
+function updateCounter() {
+    $('#count').text(`${++count} clicks on the button`);
+}
+
+const body = $('body');
+const btn = $('<button>Click here to get started</button>');
+btn.on('click', _.debounce(updateCounter, 300));
+
+body.append($('<p>ALX Dashboard</p>'));
+body.append($('<p>Dashboard data for the students</p>'));
+body.append(btn);
+body.append($('<p id=\'count\'></p>'));
+body.append($('<p>Copyright - ALX</p>'));
